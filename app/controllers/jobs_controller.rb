9e8e70ea71b3c8f6e219e6330899c219e65807ef
brickstar <brickstar@gmail.com>
2018-05-12 23:11:19 -0600

    adds edit job functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,11 +25,15 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+
+    redirect_to company_job_path
   end
 
   def destroy
--- spec/features/jobs/user_edits_job_spec.rb
+++ spec/features/jobs/user_edits_job_spec.rb
@@ -7,11 +7,11 @@ describe 'user edits job' do
         company = Company.create!(name: "ESPN")
         job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
-        visit company_job_path(job)
+        visit company_job_path(company, job)
 
-        click_on 'Edit Job'
+        click_on 'Edit'
 
-        expect(current_path).to eq(edit_company_job_path(job))
+        expect(current_path).to eq(edit_company_job_path(company, job))
 
         fill_in 'job[title]', with: 'Not Developer'
         fill_in 'job[level_of_interest]', with: 20
