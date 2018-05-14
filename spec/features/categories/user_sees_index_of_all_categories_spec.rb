fba5d208b5edbcde6429f29f7c281148ca248d13
zdcthomas <zdcthomas@yahoo.com>
2018-05-14 17:13:27 -0600

    TESTS functionality of edit and delete buttons for categories

--- spec/features/categories/user_sees_index_of_all_categories_spec.rb
+++ spec/features/categories/user_sees_index_of_all_categories_spec.rb
@@ -17,16 +17,30 @@ describe "A User" do
 
       visit categories_path
 
-      expect(page).to have_button("Delete")
-      expect(page).to have_button("Edit")
+      expect(page).to have_button("Delete_#{category1.id}")
+      expect(page).to have_button("Edit_#{category1.id}")
     end
   end
   describe 'clicks the edit button' do
     it 'should redirect to the corresponding edit page' do 
+      category1 = Category.create!(title:"Finance")
+
+      visit categories_path
+      click_on "Edit_#{category1.id}"
+
+      expect(current_path).to eq(edit_category_path category1)
     end
   end
   describe 'clicks the delete button' do
-    it 'should delete the corresponding category' do 
+    it 'should delete the corresponding category' do
+      category1 = Category.create!(title:"Finance")
+      category2 = Category.create!(title:"Entertainment Industry")
+
+      visit categories_path
+      click_on("Delete_#{category1.id}")
+
+      expect(page).to have_content(category2.title)
+      expect(page).to_not have_content(category1.title)
     end
   end
 end
