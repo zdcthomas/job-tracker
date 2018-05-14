24fbb44bc304686eca3939152a4ce15a3cd2ffdb
zdcthomas <zdcthomas@yahoo.com>
2018-05-14 14:38:43 -0600

    TESTS category creation and describes user stories for categories

---
+++ spec/features/categories/user_creates_a_category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "A User" do
+  describe "a user visits categories/new" do
+    it "should contain a form with the correct fields" do
+      visit new_category_path
+
+      expect(page).to have_content("New Category")
+      expect(page).to have_field("category[title]")
+      expect(page).to have_button("Create Category")
+    end
+    it "should add the new category to the database" do
+    end
+
+    it "should redirect to category/show page" do
+    end
+
+    it "redirects to the category creation page" do
+    end
+
+  end
+end
