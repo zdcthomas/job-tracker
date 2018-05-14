41f435fc21932e367eba7a26f64ffeec093bf12e
zdcthomas <zdcthomas@yahoo.com>
2018-05-14 17:47:03 -0600

    adds functionality to category controller WIP

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,34 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
+
   def create
+    category = Category.new(category_params)
+    if category.save
+      redirect_to category_path category
+    else
+      redirect_to new_category_path
+    end
   end
+
   def index
+    @categories= Category.all
   end
+
   def edit
+    @category = Category.find(params[:id])
   end
+
+  def update
+    @category.udpate(category_params)
+
+    
+  end
+
   def destroy
+    @category.destroy
+    
+    redirect_to 
   end
 end
