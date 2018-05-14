0cc05744573f1d9064776375b3c160d329b97e25
zdcthomas <zdcthomas@yahoo.com>
2018-05-14 15:07:21 -0600

    TESTS adds new category to database, adds database cleaner to the spec helper

--- spec/features/categories/user_creates_a_category_spec.rb
+++ spec/features/categories/user_creates_a_category_spec.rb
@@ -9,7 +9,16 @@ describe "A User" do
       expect(page).to have_field("category[title]")
       expect(page).to have_button("Create Category")
     end
+
     it "should add the new category to the database" do
+      visit new_category_path
+
+      title = "New Category"
+      fill_in "category[title]", with: title
+
+      click_on "Create"
+
+      expect(Category.find(1).title).to eq(title)
     end
 
     it "should redirect to category/show page" do
@@ -17,6 +26,5 @@ describe "A User" do
 
     it "redirects to the category creation page" do
     end
-
   end
 end
