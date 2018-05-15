9bcd1a403d4522c5dbeaaf458c71fd9ea70e67b6
zdcthomas <zdcthomas@yahoo.com>
2018-05-15 12:45:23 -0600

    adds comments route create

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :categories
+  resources :comments, only: [:create]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
