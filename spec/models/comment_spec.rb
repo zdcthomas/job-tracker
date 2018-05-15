7c5b66d9ceafe3208dfd2351e3a2a8760ad4a421
zdcthomas <zdcthomas@yahoo.com>
2018-05-15 09:02:45 -0600

    TESTS comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Comment do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = Comment.new
+        expect(comment).to be_invalid
+      end
+    end
+    context "valid attributes" do
+      it 'should be valid with content' do 
+        comment = Comment.new(content: "this is some content for a great comment")
+        expect(comment).to be_valid
+      end
+    end
+  end
+end
