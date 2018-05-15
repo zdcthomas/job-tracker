2f49240e8c22792b31d2d45d0c52ba1beb691bc5
brickstar <brickstar@gmail.com>
2018-05-14 20:01:01 -0600

    TESTS jobs category dropdown menu

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "User creates a new job" do
   scenario "a user can create a new job" do
     company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'sick')
     visit new_company_job_path(company)
 
     title = "Developer"
@@ -13,7 +14,8 @@ describe "User creates a new job" do
     fill_in "job[title]", with: title
     fill_in "job[description]", with: description
     fill_in "job[level_of_interest]", with: level_of_interest
-    fill_in "job[city]", with: city 
+    fill_in "job[city]", with: city
+    select category.title, from: 'Category'
 
     click_button "Create"
 
@@ -22,5 +24,20 @@ describe "User creates a new job" do
     expect(page).to have_content(title)
     expect(page).to have_content(level_of_interest)
     expect(page).to have_content(city)
+  end
+
+  describe 'user creates a new job' do
+    it 'should have a dropdown menu for categories' do
+      company = Company.create!(name: "ESPN")
+      visit new_company_job_path(company)
+
+      expect(page).to have_select('Category')
+    end
+    it 'should have a create new category button' do
+      company = Company.create!(name: "ESPN")
+      visit new_company_job_path(company)
+
+      expect(page).to have_link('add new category')
+    end
   end
 end
