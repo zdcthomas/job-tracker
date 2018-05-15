4c1317ae73ad2e6ea114abe3f3c5966eae426866
brickstar <brickstar@gmail.com>
2018-05-14 20:18:48 -0600

    TESTS job category relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514232118) do
+ActiveRecord::Schema.define(version: 20180515021325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,11 @@ ActiveRecord::Schema.define(version: 20180514232118) do
     t.datetime "updated_at", null: false
     t.bigint "company_id"
     t.string "city"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_jobs_on_category_id"
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
+  add_foreign_key "jobs", "categories"
   add_foreign_key "jobs", "companies"
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -33,5 +33,10 @@ describe Job do
       job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
       expect(job).to respond_to(:company)
     end
+
+    it "belongs to a category" do
+      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+      expect(job).to respond_to(:category)
+    end
   end
 end
