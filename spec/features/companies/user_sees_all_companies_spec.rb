048807bc698cb07f85226fb38bbc7ebc9105cc45
zdcthomas <zdcthomas@yahoo.com>
2018-05-16 10:07:25 -0600

    adds tests for create a new company link on company index

--- spec/features/companies/user_sees_all_companies_spec.rb
+++ spec/features/companies/user_sees_all_companies_spec.rb
@@ -10,5 +10,16 @@ describe "User sees all companies" do
     expect(page).to have_content(company.name)
     expect(page).to have_content(company_two.name)
   end
+  it 'should have a link to creating a new company at the top' do
+    company = Company.create!(name: "ESPN")
+    company_two = Company.create!(name: "Disney")
 
+    visit companies_path
+
+    expect(page).to have_link("Create A New Company")
+
+    click_on("Create A New Company")
+
+    expect(current_path).to eq(new_company_path)
+  end
 end
