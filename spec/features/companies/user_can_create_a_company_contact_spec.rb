67caad699e5cce6081d18da2eac8ba411ff756aa
brickstar <brickstar@gmail.com>
2018-05-15 20:43:26 -0600

    TESTS contact creation on company show page

--- spec/features/companies/user_can_create_a_company_contact_spec.rb
+++ spec/features/companies/user_can_create_a_company_contact_spec.rb
@@ -7,6 +7,20 @@ describe 'a user' do
     it 'can create a contact' do
     visit company_path(company)
 
+    full_name = 'Jon Doe'
+    position = 'Farmer'
+    email = 'jon@jondoe.com'
+
+    fill_in 'contact[full_name]', with: full_name
+    fill_in 'contact[position]', with: position
+    fill_in 'contact[email]', with: email
+
+    click_on 'Save'
+
+    expect(current_path).to eq(company_path)
+    expect(page).to have_content(full_name)
+    expect(page).to have_content(position)
+    expect(page).to have_content(email)
     end
   end
 end
