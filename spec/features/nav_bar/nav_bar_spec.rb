4c5b0e6b0ea381693fc25d41b5de417d5b3632c4
brickstar <brickstar@gmail.com>
2018-05-16 12:28:38 -0600

    fixes nav tests

--- spec/features/nav_bar/nav_bar_spec.rb
+++ spec/features/nav_bar/nav_bar_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe "The Nav Bar" do
   it 'should have links to jobs, companies, categories' do
+
     visit jobs_path
-    # expect(partials:"nav").to have_link("Jobs")
-    # expect(partials:"nav").to have_link("Companies")
-    # expect(partials:"nav").to have_link("Categories")
-    within('.nav_bar') do
+
+    within ('.nav_bar') do
       expect(page).to have_link('Jobs')
       expect(page).to have_link('Companies')
       expect(page).to have_link('Categories')
@@ -14,21 +13,24 @@ describe "The Nav Bar" do
   end
   context 'links' do
     it 'should redirect to jobs path' do
-      skip
-      render(partials:"nav")
+      visit companies_path
+
       click_on "Jobs"
+
       expect(current_path).to eq(jobs_path)
     end
     it 'should redirect to companies path' do
-      skip
-      render(partials:"nav")
+      visit jobs_path
+
       click_on "Companies"
+
       expect(current_path).to eq(companies_path)
     end
     it 'should redirect to Categories path' do
-      skip
-      render(partials:"nav")
+      visit jobs_path
+      
       click_on "Categories"
+
       expect(current_path).to eq(categories_path)
     end
   end
