05fa5252044703ce2568d2381a511371ed6052df
zdcthomas <zdcthomas@yahoo.com>
2018-05-16 11:06:43 -0600

    adds css styling for some elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
 Company.destroy_all
 Job.destroy_all
 
+
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Business", "High Paying", "Development", "Local", "Remote"]
+
+CATEGORIES.each do |category|
+  Category.create!(title: category)
+end
 
 COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
-    puts "  Created #{company.jobs[num].title}"
+    company.jobs.create!(title: JOBS.sample, 
+                         description: "What a great position!", 
+                         level_of_interest: num + rand(100), 
+                         category_id: Category.all.sample.id,
+                         city: CITIES.sample)
+    puts "Created #{company.jobs[num].title}"
   end
 end
