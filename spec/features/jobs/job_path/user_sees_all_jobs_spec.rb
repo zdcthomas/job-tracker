fefc9cc11b7223f60e6fc9dcdb8a739aa248ab39
zdcthomas <zdcthomas@yahoo.com>
2018-05-16 10:24:59 -0600

    TEST job show page content

--- spec/features/jobs/job_path/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/job_path/user_sees_all_jobs_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-describe "User sees all jobs from company job path" do
+describe "User sees all jobs job path" do
   scenario "a user sees all the jobs for a specific company" do
     category = Category.create!(title: 'Category')
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
-    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: category.id)
+    job1=company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+    job2=company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: category.id)
 
-    visit company_path(company)
+    visit jobs_path
 
-    expect(page).to have_content(company.name)
-    expect(page).to have_content(company.jobs.first.title)
-    expect(page).to have_content(company.jobs.last.title)
+    expect(page).to have_content(job1.title)
+    expect(page).to have_content(job1.level_of_interest)
+    expect(page).to have_content(job1.city)
+    
+    expect(page).to have_content(job2.title)
+    expect(page).to have_content(job2.level_of_interest)
+    expect(page).to have_content(job2.city)
   end
 end
